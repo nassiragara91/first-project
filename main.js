0344bc8d9f87e3bf16cbc3189a56539c9c1c3f04
nassiragara91 <garanassira@gmail.com>
2024-11-14 14:48:50 +0100

    project

---
+++ main.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+    $("#instagram").click(function() {
+        window.location.replace("https://www.instagram.com/")
+    });})
+    $("#facebook").click(function(){
+        window.location.replace("https://www.facebook.com/login.php/")
+    })
+   
